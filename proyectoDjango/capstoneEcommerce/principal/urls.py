7cd3a3ae0d9ffc60cd15177d0370983dc4f5628a
RadioRek <nic.ortega@duocuc.cl>
2025-09-01 07:54:54 -0400

    creando proyecto y estructura basica

---
+++ proyectoDjango/capstoneEcommerce/principal/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # aqui se agregan las urls de la app principal (las rutas que se ingresan en el navegador)
+    # se le indica la url de la ruta, la funcion del archivo views.py que se va a ejecutar y un nombre para la ruta para identificarla
+]
