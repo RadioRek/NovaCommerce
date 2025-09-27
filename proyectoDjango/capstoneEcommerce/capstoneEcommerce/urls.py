1e12589efaa080561f83d78e36bcba49f5dc38c6
RadioRek <nic.ortega@duocuc.cl>
2025-09-27 08:57:27 -0300

    Crazy crazy

--- proyectoDjango/capstoneEcommerce/capstoneEcommerce/urls.py
+++ proyectoDjango/capstoneEcommerce/capstoneEcommerce/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# temporal, en produccion se cambia por nginx (el + despues de urlpatterns tambien se quita)
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('principal.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- proyectoDjango/capstoneEcommerce/principal/urls.py
+++ proyectoDjango/capstoneEcommerce/principal/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
 from . import views
 
+# cositas de la API
+from rest_framework.routers import DefaultRouter
+from .views import ProductoViewSet
+router = DefaultRouter()
+router.register(r'productos', ProductoViewSet, basename="producto")
+
+
 urlpatterns = [
     # aqui se agregan las urls de la app principal (las rutas que se ingresan en el navegador)
     # se le indica la url de la ruta, la funcion del archivo views.py que se va a ejecutar y un nombre para la ruta para identificarla
@@ -12,4 +19,11 @@ urlpatterns = [
     path('sitioLogin/', views.sitioLogin, name='sitioLogin'),
     path('tienda/', views.tienda, name='tienda'),
     path('producto/<int:producto_id>/', views.producto, name='producto'),
+
+
+    # rutas de la API
+    path('api/', include(router.urls)),
 ]
+
+
+
