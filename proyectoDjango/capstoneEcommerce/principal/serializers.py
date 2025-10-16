32d616c164fcef299db9436927e6c847476560a8
RadioRek <nic.ortega@duocuc.cl>
2025-10-16 15:51:40 -0300

    merges me vuelvo loco

--- proyectoDjango/capstoneEcommerce/principal/serializers.py
+++ proyectoDjango/capstoneEcommerce/principal/serializers.py
@@ -48,15 +48,4 @@ class UserSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
-    # Puedes agregar validaciones personalizadas si es necesario
-    def validate_precio(self, value):
-        if value < 0:
-            raise serializers.ValidationError("El precio no puede ser negativo.")
-        return value
-    # se usa validate_<NOMBRE DEL CAMPO A VALIDAR>
-    # si la validacion falla, la api respondera con un error 400 (bad request)
 
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
